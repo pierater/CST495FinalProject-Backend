98eba05bd782a4af58995323e1503ad3243b9367
438PR <preinsch@csumb.edu>
2016-09-29 21:42:35 -0700

    android beginnings

---
+++ android/Router/app/src/main/java/edu/csumb/cst438/router/MyRoutes.java
@@ -0,0 +1,13 @@
+package edu.csumb.cst438.router;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyRoutes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_routes);
+    }
+}
