59afdb3ec23b3f2d8811c8913e3f21ea888a3d22
martin almaraz <martin.almaraz@gmail.com>
2016-10-13 15:20:48 -0700

    Attempting to fix circleci testing

--- router/src/test/preTest.py
+++ router/src/test/preTest.py
@@ -1,11 +1,5 @@
 #!/bin/env python3.5
 import sys
-'''
-sys.path.append('.')
-sys.path.append('../rest')
-sys.path.append('../rest/endpoints')
-sys.path.append('../rest/dbUtil')
-'''
 
 sys.path.append('.')
 sys.path.append('./src')
