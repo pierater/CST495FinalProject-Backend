0c22def6069be62aa9717ba9c5c6d3c750adfe46
438PR <preinsch@csumb.edu>
2016-10-19 16:51:37 -0700

    still working on the type error

--- router/src/test/getNearMe_test.py
+++ router/src/test/getNearMe_test.py
@@ -15,10 +15,8 @@ class TestGetNearMe():
 	dist = "10"
 	route1 = "this is a route XD"
 	route2 = "this is another route XP"
-	routeId1str = "-1"
-	routeId2str = "-1"
-	routeId1int = -1
-	routeId2int = -1
+	routeId1 = "-1"
+	routeId2 = "-1"
 	startPointLat1 = "36.67"
 	startPointLat2 = "-12.8"
 	startPointLon1 = "-121.81"
@@ -29,14 +27,14 @@ class TestGetNearMe():
 
 	def test_getNearMe(self):
 		response = json.loads(getNearMe.getNearMe(self.userLat, self.userLon, self.dist))
-		assert response[0]['idroutes'] != self.routeId2int
-		assert response[0]['idroutes'] == self.routeId1int
+		assert response[0]['idroutes'] != self.routeId2
+		assert response[0]['idroutes'] == self.routeId1
 
 	def setup_method(self):
-		self.routeId1str = dbconnect.insert_data_routes(self.route1,self.startPointLat1,self.startPointLon1,self.userid,self.routeName1)
-		self.routeId2str = dbconnect.insert_data_routes(self.route2,self.startPointLat2,self.startPointLon2,self.userid,self.routeName2)
-		self.routeId1int = self.routeId1str[0]['idroutes']
-		self.routeId2int = self.routeId2str[0]['idroutes']
+		self.routeId1 = dbconnect.insert_data_routes(self.route1,self.startPointLat1,self.startPointLon1,self.userid,self.routeName1)
+		self.routeId2 = dbconnect.insert_data_routes(self.route2,self.startPointLat2,self.startPointLon2,self.userid,self.routeName2)
+		self.routeId1 = int(self.routeId1[0]['idroutes'])
+		self.routeId2 = int(self.routeId2[0]['idroutes'])
 
 	def teardown_method(self):
 		dbconnect.delete_data('routes', 'userid', self.userid)
