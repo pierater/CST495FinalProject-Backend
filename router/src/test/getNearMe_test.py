2236be12d1c9b29aed8cfdae3340f6921726c611
438PR <preinsch@csumb.edu>
2016-10-19 16:48:08 -0700

    trying to fix test type error that only seems to happen on circleci

--- router/src/test/getNearMe_test.py
+++ router/src/test/getNearMe_test.py
@@ -15,8 +15,10 @@ class TestGetNearMe():
 	dist = "10"
 	route1 = "this is a route XD"
 	route2 = "this is another route XP"
-	routeId1 = "-1"
-	routeId2 = "-1"
+	routeId1str = "-1"
+	routeId2str = "-1"
+	routeId1int = -1
+	routeId2int = -1
 	startPointLat1 = "36.67"
 	startPointLat2 = "-12.8"
 	startPointLon1 = "-121.81"
@@ -27,14 +29,14 @@ class TestGetNearMe():
 
 	def test_getNearMe(self):
 		response = json.loads(getNearMe.getNearMe(self.userLat, self.userLon, self.dist))
-		assert response[0]['idroutes'] != self.routeId2
-		assert response[0]['idroutes'] == self.routeId1
+		assert response[0]['idroutes'] != self.routeId2int
+		assert response[0]['idroutes'] == self.routeId1int
 
 	def setup_method(self):
-		self.routeId1 = dbconnect.insert_data_routes(self.route1,self.startPointLat1,self.startPointLon1,self.userid,self.routeName1)
-		self.routeId2 = dbconnect.insert_data_routes(self.route2,self.startPointLat2,self.startPointLon2,self.userid,self.routeName2)
-		self.routeId1 = self.routeId1[0]['idroutes']
-		self.routeId2 = self.routeId2[0]['idroutes']
+		self.routeId1str = dbconnect.insert_data_routes(self.route1,self.startPointLat1,self.startPointLon1,self.userid,self.routeName1)
+		self.routeId2str = dbconnect.insert_data_routes(self.route2,self.startPointLat2,self.startPointLon2,self.userid,self.routeName2)
+		self.routeId1int = self.routeId1str[0]['idroutes']
+		self.routeId2int = self.routeId2str[0]['idroutes']
 
 	def teardown_method(self):
 		dbconnect.delete_data('routes', 'userid', self.userid)
