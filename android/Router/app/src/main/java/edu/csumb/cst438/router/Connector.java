b29241931b16ef4c565b2413ba327e54c82c0788
martin almaraz <martin.almaraz@gmail.com>
2016-10-28 17:33:56 -0700

    all endpoints are working on android
    Summary: everything is threaded and working fine
    
    Test Plan: plannnss
    
    Reviewers: laurachavez1, expertReptile, 438PR
    
    Reviewed By: 438PR
    
    Differential Revision: http://csumbse.net/D124

--- android/Router/app/src/main/java/edu/csumb/cst438/router/Connector.java
+++ android/Router/app/src/main/java/edu/csumb/cst438/router/Connector.java
@@ -2,6 +2,7 @@ package edu.csumb.cst438.router;
 
 import android.util.Log;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -12,6 +13,10 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * Created by pico on 10/15/16.
@@ -28,6 +33,7 @@ public class Connector {
     static String uploadRoute = "/uploadRoute/";
     static String createUser = "/createUser/";
     static String getNearMe = "/getNearMe/";
+    static ExecutorService executorService = Executors.newSingleThreadExecutor();
 
 
     public Connector() {
@@ -39,6 +45,103 @@ public class Connector {
         catch (Exception e) {
             Log.e("error", e.toString());
         }
+    }
+
+    public HashMap<String, String> downloadRoute(final int routeId) {
+
+        Callable<HashMap<String, String>> callable = new Callable<HashMap<String, String>>() {
+            @Override
+            public HashMap<String, String> call() throws Exception {
+                return downloadRoute_internal(routeId);
+            }
+        };
+        Future<HashMap<String, String>> future = executorService.submit(callable);
+        executorService.shutdown();
+        try {
+            return future.get();
+        }
+        catch (Exception e) {
+            Log.e("error", e.toString());
+        }
+        return null;
+    }
+
+    public int createUser(final String username, final String password, final String bio, final String email) {
+
+        Callable<Integer> callable = new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return createUser_internal(username, password, bio, email);
+            }
+        };
+        Future<Integer> future = executorService.submit(callable);
+        executorService.shutdown();
+        try {
+            return future.get();
+        }
+        catch (Exception e) {
+            Log.e("error", e.toString());
+        }
+        return -1;
+    }
+
+    public int uploadRoute(final int userId, final String lat, final String lon, final String name, final String path) {
+
+        Callable<Integer> callable = new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return uploadRoute_intenal(userId, lat, lon, name, path);
+            }
+        };
+        Future<Integer> future = executorService.submit(callable);
+        executorService.shutdown();
+        try {
+            return future.get();
+        }
+        catch (Exception e) {
+            Log.e("error", e.toString());
+        }
+        return -1;
+    }
+
+    public ArrayList<HashMap<String, String>> getNearMe(final String lat, final String lon, final double distance) {
+
+        Callable<ArrayList<HashMap<String, String>>> callable = new Callable<ArrayList<HashMap<String, String>>>() {
+            @Override
+            public ArrayList<HashMap<String, String>> call() throws Exception {
+                return getNearMe_internal(lat, lon, distance);
+            }
+        };
+
+        Future<ArrayList<HashMap<String, String>>> future = executorService.submit(callable);
+        executorService.shutdown();
+        try {
+            return future.get();
+        }
+        catch (Exception e) {
+            Log.e("error", e.toString());
+        }
+        return null;
+    }
+
+    public boolean checkLogin(final String username, final String password) {
+
+        Callable<Boolean> callable = new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                return checkLogin_internal(username, password);
+            }
+        };
+        Future<Boolean> future = executorService.submit(callable);
+        executorService.shutdown();
+        try {
+            return future.get();
+        }
+        catch (Exception e) {
+            Log.e("error", e.toString());
+        }
+        return false;
+
     }
 
     private HttpURLConnection buildConnection(String endpoint) {
@@ -94,7 +197,7 @@ public class Connector {
     }
 
 
-    public int uploadRoute(int userId, String lat, String lon, String name, String path) {
+    private int uploadRoute_intenal(int userId, String lat, String lon, String name, String path) {
         HashMap<String, String> result = new HashMap<String, String>();
         String json = "";
 
@@ -134,7 +237,7 @@ public class Connector {
     }
 
 
-    public ArrayList<HashMap<String, String>> getNearMe(String lat, String lon, double distance) {
+    private ArrayList<HashMap<String, String>> getNearMe_internal(String lat, String lon, double distance) {
         ArrayList<HashMap<String, String>> result = new ArrayList<HashMap<String, String>>();
         String json = "";
 
@@ -149,15 +252,16 @@ public class Connector {
         }
 
         try {
-            JSONObject response = new JSONObject(getResponse(json.toString(), getNearMe));
+            JSONArray response = new JSONArray(getResponse(json.toString(), getNearMe));
 
             for(int i = 0; i < response.length(); i++) {
                 HashMap<String, String> temp = new HashMap<>();
-                Iterator<?> keys = response.keys();
+                JSONObject item = (JSONObject)response.get(i);
+                Iterator<?> keys = item.keys();
 
                 while(keys.hasNext()) {
                     String key = keys.next().toString();
-                    temp.put(key, response.get(key).toString());
+                    temp.put(key, item.get(key).toString());
                 }
                 result.add(temp);
             }
@@ -169,7 +273,7 @@ public class Connector {
         return result;
     }
 
-    public HashMap<String, String> downloadRoute(int routeId) {
+    private HashMap<String, String> downloadRoute_internal(int routeId) {
         HashMap<String, String> result = new HashMap<>();
         String json = "";
         try {
@@ -196,7 +300,7 @@ public class Connector {
         return result;
     }
 
-    public int createUser(String username, String password, String bio, String email) {
+    private int createUser_internal(String username, String password, String bio, String email) {
         password = sha1(password);
         String json = "";
         try {
@@ -220,7 +324,7 @@ public class Connector {
         return -1;
     }
 
-    public Boolean checkLogin(String username, String password) {
+    private Boolean checkLogin_internal(String username, String password) {
         password = sha1(password);
         String json = "";
         try {
