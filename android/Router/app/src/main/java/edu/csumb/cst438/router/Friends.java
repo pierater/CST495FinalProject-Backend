73655a72cc6a747c5b190e98a990307a14db5756
438PR <preinsch@csumb.edu>
2016-11-03 18:11:48 -0700

    a menu drawer with clickable menu tiles
    Summary: Nearly all activities have been modified to implement a menu drawer.
    
    Test Plan: Test that the different menu clicks go to the correct page
    
    Reviewers: malmaraz, expertReptile, laurachavez1
    
    Subscribers:

--- android/Router/app/src/main/java/edu/csumb/cst438/router/Friends.java
+++ android/Router/app/src/main/java/edu/csumb/cst438/router/Friends.java
@@ -1,13 +1,26 @@
 package edu.csumb.cst438.router;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 
 public class Friends extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.friends_drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.friends_left_drawer);
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.menu_string_array)));
+
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
 }
