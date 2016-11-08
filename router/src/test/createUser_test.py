f0cdd2458c8564b7ecc5ded9664495f16e271a1f
laurachavez1 <lachavez@csumb.edu>
2016-11-07 22:23:46 -0800

    Added create user endpoint test
    Summary:
    
    Test Plan: Run tests to make sure they still pass. Will add more create user
    tests, with no email/password/username
    
    Reviewers: malmaraz, expertReptile, 438PR
    
    Subscribers:

--- router/src/test/createUser_test.py
+++ router/src/test/createUser_test.py
@@ -6,8 +6,8 @@ import dbconnect
 '''
 Author: Laura Chavez
 Description: A test that tests createUser endpoint
+Updated: 7 November, 2016
 '''
-
 
 username = 'testuser'
 password = 's3cr3t'
@@ -17,14 +17,20 @@ email = 'email@mail.com'
 # Tests that a user can be created.
 def test_createUser():
     userid = dbconnect.insert_data_users(username, bio, password, email)
-    print (userid)
     assert userid is not None
+
 
 # Test if username is already taken, user can't be created
 def test_createExistingUser():
     dbconnect.insert_data_users(username, bio, password, email)
     userid = dbconnect.insert_data_users(username, bio, password, email)
     assert userid == -1
+
+# Test that user can still be created with empty bio
+def test_createUserWithNoBio():
+    emptyBio = ""
+    userid = dbconnect.insert_data_users(username, emptyBio, password, email)
+    assert userid is not None
 
 def teardown_function():
     dbconnect.delete_data('users', 'username', username)
