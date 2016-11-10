d789ab83d060dd1c487a17c1c663fc46f4546928
438PR <preinsch@csumb.edu>
2016-11-09 20:32:52 -0800

    adding swipeable list items
    Summary: friends and myRoutes now have swipable list objects which reveal buttons when swiped
    
    Test Plan: Test that the buttons perform the functionality they describe
    
    Reviewers: malmaraz, expertReptile, laurachavez1
    
    Subscribers:

--- android/Router/app/src/main/java/edu/csumb/cst438/router/MyRoutes.java
+++ android/Router/app/src/main/java/edu/csumb/cst438/router/MyRoutes.java
@@ -3,30 +3,56 @@ package edu.csumb.cst438.router;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
 import com.arlib.floatingsearchview.FloatingSearchView;
+import com.arlib.floatingsearchview.suggestions.model.SearchSuggestion;
 
 public class MyRoutes extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
     private FloatingSearchView mSearchView;
+    private TextView noDataMyRoutes;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mRecyclerAdapter;
+
+    private String myRoutes[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_routes);
-
+        //      TODO: make proper call to DB to get the list of the User's friends
+        myRoutes = new String[] {"Friend1","Friend2","Friend3","Friend4","Friend5","Friend6","Friend7","Friend8","Friend9","Friend10",};
+        noDataMyRoutes = (TextView) findViewById(R.id.no_data_myRoutes);
+        mRecyclerView = (RecyclerView) findViewById(R.id.myRoutes_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerAdapter= new SwipeRecyclerViewAdapter(this, myRoutes);
+        mRecyclerView.setAdapter(mRecyclerAdapter);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.myRoutes_drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.myRoutes_left_drawer);
         mSearchView = (FloatingSearchView) findViewById(R.id.floating_search_view);
-
         mDrawerList.setAdapter(new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.menu_string_array)));
-
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-
         mSearchView.attachNavigationDrawerToMenuButton(mDrawerLayout);
+
+        mSearchView.setOnSearchListener(new FloatingSearchView.OnSearchListener() {
+            @Override
+            public void onSuggestionClicked(final SearchSuggestion searchSuggestion) {}
+
+            @Override
+            public void onSearchAction(String query) {
+
+                //TODO: figure out how to use results
+                System.out.println(SearchEngine.findFriends(query));
+            }
+        });
     }
 }
