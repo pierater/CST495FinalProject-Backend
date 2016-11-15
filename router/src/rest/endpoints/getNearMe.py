b2cf5abca9ab1ee27f33f5227e736fc3b05dfeb9
438PR <preinsch@csumb.edu>
2016-11-14 18:48:07 -0800

    Dead Code - Variable Extraction - Added Try Block
    Summary: more cleanup for getNearMe.py, made dead code accessable by adding try block
    
    Test Plan: tested by getNearMe_test.py
    
    Reviewers: malmaraz, expertReptile, laurachavez1
    
    Subscribers:

--- router/src/rest/endpoints/getNearMe.py
+++ router/src/rest/endpoints/getNearMe.py
@@ -14,15 +14,19 @@ getNearMeBlueprint = Blueprint('getNearMe', __name__, template_folder='templates
 # takes a user's location (latitude & longitude) and a distance (in KM)
 # returns a list of routes with a start point within the given distance of the user's location
 def getNearMe(userLatitude = None, userLongitude = None, preferredDistance = None):
-
-	if userLatitude is None or userLongitude is None:
-		userLatitude = request.json['userLat']
-		userLongitude = request.json['userLon']
-		preferredDistance = request.json['dist']
-
+	isThisAProductionRun = userLatitude is None or userLongitude is None
+	
 	query = "SELECT idroutes, route, ( 3959 * acos( cos( radians(" + userLatitude
 	query += " ) ) * cos( radians( startPointLat ) ) * cos( radians( startPointLon ) - radians(" + userLongitude
 	query += " ) ) + sin( radians(" + userLatitude + ") ) * sin( radians( startPointLat ) ) ) ) "
 	query += "AS distance FROM routes HAVING distance < " + str(preferredDistance) + " ORDER BY distance LIMIT 0 , 20" 
-	cursor = dbconnect.__change_data(query,preferredDistance)
-	return json.dumps(cursor)
+
+	try:
+		assert (isThisAProductionRun == True)
+	except AssertionError:
+		userLatitude = request.json['userLat']
+		userLongitude = request.json['userLon']
+		preferredDistance = request.json['dist']
+	finally:
+		cursor = dbconnect.__change_data(query,preferredDistance)
+		return json.dumps(cursor)
