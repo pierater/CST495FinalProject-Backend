59f9b603b0eb9eefb5902b5e49aa26f2161d52a6
438PR <preinsch@csumb.edu>
2016-11-14 17:37:17 -0800

    Deodorant - Rename Variables - Delete Comments
    Summary: Fixing code smell
    
    Test Plan: tested by getNearMe_test.py
    
    Reviewers: malmaraz, expertReptile, laurachavez1
    
    Subscribers:

--- router/src/rest/endpoints/getNearMe.py
+++ router/src/rest/endpoints/getNearMe.py
@@ -6,15 +6,6 @@ import dbconnect
 '''
 Description: Rest endpoint for retrieving nearby routes
 Author: Pearce Reinsch
-
-	~ routes Table ~
-	
-	idroutes INT NOT NULL AUTO_INCREMENT, 
-	route VARCHAR(1000) NOT NULL,
-	startPointLat VARCHAR(10) NOT NULL,
-	startPointLon VARCHAR(10) NOT NULL,
-	userid VARCHAR(45) NOT NULL, 
-	
 '''
 
 
@@ -22,36 +13,16 @@ getNearMeBlueprint = Blueprint('getNearMe', __name__, template_folder='templates
 @getNearMeBlueprint.route("/getNearMe/", methods=['POST'])
 # takes a user's location (latitude & longitude) and a distance (in KM)
 # returns a list of routes with a start point within the given distance of the user's location
-def getNearMe(userLat = None, userLon = None, dist = None):
-	'''
-	This query requires: 
-	userLat (the user's latitude coordinate ie: 80.12)
-	userLon (the user's longitude coordinate ie: -46.49)
-	dist	(the user's preferred search distance in KM ie: 10)
-	'''
-	if userLat is None or userLon is None:
-		userLat = request.json['userLat']
-		userLon = request.json['userLon']
-		dist = request.json['dist']
+def getNearMe(userLatitude = None, userLongitude = None, preferredDistance = None):
 
-	query = "SELECT idroutes, route, ( 3959 * acos( cos( radians(" + userLat
-	query += " ) ) * cos( radians( startPointLat ) ) * cos( radians( startPointLon ) - radians(" + userLon
-	query += " ) ) + sin( radians(" + userLat + ") ) * sin( radians( startPointLat ) ) ) ) "
-	query += "AS distance FROM routes HAVING distance < " + str(dist) + " ORDER BY distance LIMIT 0 , 20" 
-	cursor = dbconnect.__change_data(query,dist)
-	
-	'''
-	The returned object is structured as a list of JSON objects
-	ie:
-	
-	[{'route': '***', 'idroutes': 50, 'distance': 1.488970881174448}, 
-	{'route': '***', 'idroutes': 51, 'distance': 9648.624280895436}]
-	
-	this example has 2 route JSON objects with the accessible values being 
-	'route' 'idroutes' and 'distance'
-	
-	these values are the actual route points, the id of the route, and the distance from the
-	user's entered coordinates
-	'''
-	
+	if userLatitude is None or userLongitude is None:
+		userLatitude = request.json['userLat']
+		userLongitude = request.json['userLon']
+		preferredDistance = request.json['dist']
+
+	query = "SELECT idroutes, route, ( 3959 * acos( cos( radians(" + userLatitude
+	query += " ) ) * cos( radians( startPointLat ) ) * cos( radians( startPointLon ) - radians(" + userLongitude
+	query += " ) ) + sin( radians(" + userLatitude + ") ) * sin( radians( startPointLat ) ) ) ) "
+	query += "AS distance FROM routes HAVING distance < " + str(preferredDistance) + " ORDER BY distance LIMIT 0 , 20" 
+	cursor = dbconnect.__change_data(query,preferredDistance)
 	return json.dumps(cursor)
