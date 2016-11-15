4e8c9cad763695be0433ee9f81bb1f63e62c745f
laurachavez1 <lachavez@csumb.edu>
2016-11-14 16:45:14 -0800

    Code Smell: Comments     - Changed Parameter Names to fix comment issues Code Smell: Duplicated Code
    Summary: Removed unnecessary comments from the file,
    Removed duplicated code that was never used.
    
    Test Plan: Tests should still be passing
    
    Reviewers: 438PR, expertReptile, malmaraz
    
    Differential Revision: http://csumbse.net/D338

--- router/src/rest/dbUtil/dbconnect.py
+++ router/src/rest/dbUtil/dbconnect.py
@@ -13,53 +13,36 @@ LOCAL = './src/rest/dbUtil/config.ini'
 dbConfig=LOCAL
 
 # Inserting data to Users Table
-# PARAM1: username value
-# PARAM2: bio value
-# PARAM3: passwd (NOT HASHED IN FUNCTION)
-# Returns new user's id
-def insert_data_users(username,bio,passwd,email):
+def insert_data_users(username, bio, password, email):
     if( len(get_field("idusers", "users", "username", username)) > 0): return -1
     query = "INSERT INTO users(idusers,username,bio,pass,email) " \
         "VALUES(NULL,%s,%s,%s,%s)"
-    args = (username, bio, passwd, email)
-    error = __change_data(query,args)
+    args = (username, bio, password, email)
+    __change_data(query,args)
     userid = get_field("idusers","users","username",username)
     return userid[0]['idusers']
 
 # Inserting data to Routes Table
-# PARAM1: route value
-# PARAM2: route start point latitude
-# PARAM3: route start point longitude
-# PARAM4: userid value, user that the route belongs to
-def insert_data_routes(route,startPointLat,startPointLon,userid,routeName):
+def insert_data_routes(route, startPointLatitude, startPointLongitude, userid, routeName):
     if( len(get_field("idroutes", "routes", "route", route)) > 0): return -1
 
     query = "INSERT INTO routes(route,startPointLat,startPointLon,userid,routeName) " \
         "VALUES(%s,%s,%s,%s,%s)"
-    args = (route, startPointLat, startPointLon, userid, routeName)
+    args = (route, startPointLatitude, startPointLongitude, userid, routeName)
     __change_data(query,args)
 
     return get_field("idroutes", "routes", "route", route)
 
-
-# OPTING OUT COMMENTS TABLE FOR NOW..
-
 # Update a single field in specified table
-# PARAM1: Name of the table that will be updated
-# PARAM2: Name of field that's value will be changed
-# PARAM3: New value for field specified in (PARAM2)
-# PARAM4 & PARAM5: Condition for where statement (field, value) respectively
-def update_data(tablename, setfield, newvalue, wherefield, cond):
-    query = "UPDATE %s SET %s = %s WHERE %s = %s" % (tablename, setfield, '%s', wherefield, '%s')
-    args = (newvalue,cond)
+def update_data(tableName, fieldName, newValue, fieldNameForCondition, valueForCondition):
+    query = "UPDATE %s SET %s = %s WHERE %s = %s" % (tableName, fieldName, '%s', fieldNameForCondition, '%s')
+    args = (newValue, valueForCondition)
     __change_data(query,args)
 
 # Deletes row from specified table
-# PARAM1: Name of table where row is deleted from
-# PARAM2 & PARAM3: Condition for where statement (field, value) respectively
-def delete_data(tablename, wherefield, condition):
-    query = "DELETE FROM %s WHERE %s = %s" % (tablename, wherefield, '%s')
-    args = (condition,)
+def delete_data(tableName, fieldNameForCondition, valueForCondition):
+    query = "DELETE FROM %s WHERE %s = %s" % (tableName, fieldNameForCondition, '%s')
+    args = (valueForCondition,)
     __change_data(query,args)
 
 # Connects to database and processes the query
@@ -78,42 +61,18 @@ def __change_data(query,args):
             print(error)
     except Error as error:
         print(error)
-################ TESTING ##################
-# USED FOR TESTING
-def get_field(fieldname, tablename,fieldnamecondition,fieldvaluecondition):
 
+def get_field(fieldName, tableName, fieldNameForCondition, valueForCondition):
     try:
         db_config = read_db_config(dbConfig)
         conn = MySQLConnection(**db_config)
         cursor = conn.cursor(dictionary=True)
         
-        query = "SELECT %s FROM %s WHERE %s = %s" % (fieldname,tablename,fieldnamecondition, '%s')
-        args = (fieldvaluecondition,)
+        query = "SELECT %s FROM %s WHERE %s = %s" % (fieldName,tableName,valueForCondition, '%s')
+        args = (valueForCondition,)
         cursor.execute(query,args)
         
         row = cursor.fetchall()
         return row
     except Error as error:
         print(error)
-    
-def testInsert():
-    insert_data_users("user123", "bioInfo","pw098")
-    assert (get_field("idusers", "users","username","user123") > 0 ),"Insert Users error"
-    delete_data("users","username","user123") # Deleting for in case test is ran again
-    
-    insert_data_routes("rtemp","1")
-    assert (get_field("idroutes","routes","route","rtemp") > 0),"Insert Routes error"
-    insert_data_routes("rtemp2","1")
-    assert (get_field("idroutes","routes","route","rtemp2") > 0),"Insert Routes error"
-    delete_data("routes","route","rtemp")
-    delete_data("routes","route","rtemp2")
-
-def testUpdate():
-    insert_data_users("user123", "bioInfo","pw098")
-    update_data("users", "username", "user4", "username", "user123")
-    assert(get_field("username","users","username", "user4") > 0), "Update Users Error"
-    delete_data("users","username","user4")
-
-if __name__ == '__main__':
-    testInsert()
-    testUpdate()
