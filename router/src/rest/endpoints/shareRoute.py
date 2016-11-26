e4c2f6045f08506df7375d7d25912d9ba9d226b9
438PR <preinsch@csumb.edu>
2016-11-25 20:52:56 -0800

    updating shareRoute.py
    Summary: updating shareRoute.py
    
    Test Plan: tbd
    
    Reviewers: malmaraz, expertReptile, laurachavez1
    
    Subscribers:

--- router/src/rest/endpoints/shareRoute.py
+++ router/src/rest/endpoints/shareRoute.py
@@ -2,25 +2,21 @@
 import json
 from flask import Blueprint, request
 import dbconnect
+import codes
 
 '''
 Description: Rest endpoint for sharing routes between friends
 Author: Pearce Reinsch
 '''
 
-
 shareRouteBlueprint = Blueprint('router', __name__, template_folder='templates')
 @shareRouteBlueprint.route("/shareRoute/", methods=['POST'])
 # takes a user's Id and desired Route and stores the share and route on the remote DB
 # returns a success/failed response
 def shareRoute(meId = None, youId = None, route = None, routeId = None, routeName = None, startLatitude = None, startLongitude = None):
-	isThisAProductionRun = meId is None
-	
 	query = "INSERT INTO shared(toId, fromId, routeId) VALUES(%s,%s,%s)"
 
-	try:
-		assert (isThisAProductionRun == True)
-	except AssertionError:
+	if(meId is None):
 		meId = request.json['meId']
 		youId = request.json['youId']
 		route = request.json['route']
@@ -28,11 +24,11 @@ def shareRoute(meId = None, youId = None, route = None, routeId = None, routeNam
 		routeName = request.json['routeName']
 		startLatitude = request.json['startLatitude']
 		startLongitude = request.json['startLongitude']
-	finally:
-		args = (youId, meId, routeId)
-		try:
-			dbconnect.__change_data(query,args)
-			dbconnect.insert_data_routes(route,startLatitue,startLongitude,meId,routeName)
-			return json.dumps(codes.SUCCESS)
-		except Exception as e:
-			return json.dumps(codes.FAILURE)
+		
+	args = (youId, meId, routeId)
+	try:
+		dbconnect.__change_data(query,args)
+		dbconnect.insert_data_routes(route,startLatitue,startLongitude,meId,routeName)
+		return json.dumps(codes.SUCCESS)
+	except Exception as e:
+		return json.dumps(codes.FAILURE)
