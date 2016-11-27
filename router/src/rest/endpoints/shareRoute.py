32b8b9587932b92a412b22d52f24151579447d90
438PR <preinsch@csumb.edu>
2016-11-27 14:46:46 -0800

    added 3 endpoints to app.py
    Summary: Added getRoutesShared, shareRoute, & processRequest to app.py
    
    Test Plan: tbd
    
    Reviewers: expertReptile, malmaraz, laurachavez1
    
    Reviewed By: malmaraz, laurachavez1
    
    Differential Revision: http://csumbse.net/D472

--- router/src/rest/endpoints/shareRoute.py
+++ router/src/rest/endpoints/shareRoute.py
@@ -9,37 +9,6 @@ Description: Rest endpoint for sharing routes between friends
 Author: Pearce Reinsch
 '''
 
-<<<<<<< HEAD
-
-getNearMeBlueprint = Blueprint('router', __name__, template_folder='templates')
-@getNearMeBlueprint.route("/shareRoute/", methods=['POST'])
-# takes a user's Id and desired Route and stores the share and route on the remote DB
-# returns a success/failed response
-def getNearMe(meId = None, youId = None, route = None, routeId = None, routeName = None, startLatitude = None, startLongitude = None):
-	meId = None, youId = None, route = None, routeName = None, startLatitue = None, startLongitude = None
-	isThisAProductionRun = meId is None
-	
-	query = "INSERT INTO shared(toId, fromId, routeId) VALUES(%s,%s,%s)"
-
-	try:
-		assert (isThisAProductionRun == True)
-	except AssertionError:
-		meId = request.json['meId']
-		youId = request.json['youId']
-		route = request.json['route']
-		routeId = request.json['routeId']
-		routeName = request.json['routeName']
-		startLatitude = request.json['startLatitude']
-		startLongitude = request.json['startLongitude']
-	finally:
-		args = (youId, meId, routeId)
-		try:
-			dbconnect.__change_data(query,args)
-			dbconnect.insert_data_routes(route,startLatitue,startLongitude,meId,routeName)
-			return json.dumps(codes.SUCCESS)
-		except Exception as e:
-			return json.dumps(codes.FAILURE)
-=======
 shareRouteBlueprint = Blueprint('router', __name__, template_folder='templates')
 @shareRouteBlueprint.route("/shareRoute/", methods=['POST'])
 # takes a user's Id and desired Route and stores the share and route on the remote DB
@@ -63,4 +32,3 @@ def shareRoute(sender_id = None, receiver_id = None, route = None, route_id = No
 		return json.dumps(codes.SUCCESS)
 	except Exception as e:
 		return json.dumps(codes.FAILURE)
->>>>>>> 6238ee8db728ef8d587fe1987cbe8d487767338e
