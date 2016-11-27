3ddc6a541ab56c6ff7a6013ebe5d8110ad963596
martin almaraz <martin.almaraz@gmail.com>
2016-11-27 15:28:18 -0800

    fixed my endpoints, they work now
    Summary: getFriends and getFriendRequests work now!
    
    Test Plan: TBD
    
    Reviewers: expertReptile, 438PR, laurachavez1
    
    Reviewed By: laurachavez1
    
    Differential Revision: http://csumbse.net/D473

--- router/src/rest/endpoints/getFriends.py
+++ router/src/rest/endpoints/getFriends.py
@@ -9,16 +9,15 @@ Author: Martin Almaraz
 Description: Endpoint to get all current friends for a single user
 '''
 
-getFriendsBlueprint = Blueprint('router', __name__, template_folder='templates')
+getFriendsBlueprint = Blueprint('getFriends', __name__, template_folder='templates')
 @getFriendsBlueprint.route("/getFriends/", methods=['POST'])
 def getFriends(userId = None):
 
     if userId is None:
         userId = request.json['userId']
 
-    query = '''SELECT `username`, `bio`, `userId` FROM 
-    `friend` INNER JOIN `users` ON friend.`friend_id` = users.`idusers`
-    AND friend.`user_id` = %s'''
+    query = '''SELECT `username`, `bio`, `idusers` FROM
+            (SELECT * FROM `users` INNER JOIN `friend` on `friend`.`friend_id` = `users`.`idusers` WHERE user_id = %s) as t;'''
 
     cursor = dbconnect.__change_data(query, (userId,))
 
