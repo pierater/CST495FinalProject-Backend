81784f6a590b69ea5a73cacbf8ff92b603673cd1
expertReptile <asoriano@csumb.edu>
2016-11-27 16:22:40 -0800

    fixed implementation of addFriend
    Summary: did things wrong, fixed them.
    
    Test Plan: same
    
    Reviewers: 438PR, malmaraz, laurachavez1
    
    Subscribers:

--- router/src/rest/endpoints/addFriend.py
+++ router/src/rest/endpoints/addFriend.py
@@ -12,17 +12,17 @@ addFriendBlueprint = Blueprint('addFriend', __name__, template_folder='templates
 @addFriendBlueprint.route("/addFriend/", methods=['POST'])
 def addFriend(user_id = None, friend_id = None):
     
-    query = "INSERT INTO friend(user_id, friend_id) VALUES(%s,%s)"
+    query = "INSERT INTO request(receiver_id, sender_id) VALUES(%s,%s)"
 
     if user_id is None:
         payload = request.json
         user_id = payload["user_id"]
         friend_id = payload["friend_id"]
         
-    args = (user_id, friend_id)
+    args = (friend_id, user_id)
 
     try:
-        dbconnect.__change_date(query, args)
+        dbconnect.__change_data(query, args)
         return codes.JSON_SUCCESS
     except:
         return codes.JSON_FAILURE
