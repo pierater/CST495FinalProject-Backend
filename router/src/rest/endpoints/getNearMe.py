70c8900a6325088c40c8383a9722b80e2516795e
expertReptile <asoriano@csumb.edu>
2016-11-30 14:45:58 -0800

    fixed getNearMe

--- router/src/rest/endpoints/getNearMe.py
+++ router/src/rest/endpoints/getNearMe.py
@@ -2,6 +2,7 @@
 import json
 from flask import Blueprint, request
 import dbconnect
+import codes
 
 '''
 Description: Rest endpoint for retrieving nearby routes
@@ -14,19 +15,19 @@ getNearMeBlueprint = Blueprint('getNearMe', __name__, template_folder='templates
 # takes a user's location (latitude & longitude) and a distance (in KM)
 # returns a list of routes with a start point within the given distance of the user's location
 def getNearMe(userLatitude = None, userLongitude = None, preferredDistance = None):
-	isThisAProductionRun = userLatitude is None or userLongitude is None
-	
-	query = "SELECT idroutes, route, ( 3959 * acos( cos( radians(" + userLatitude
-	query += " ) ) * cos( radians( startPointLat ) ) * cos( radians( startPointLon ) - radians(" + userLongitude
-	query += " ) ) + sin( radians(" + userLatitude + ") ) * sin( radians( startPointLat ) ) ) ) "
-	query += "AS distance FROM routes HAVING distance < " + str(preferredDistance) + " ORDER BY distance LIMIT 0 , 20" 
+    isThisAProductionRun = userLatitude is None or userLongitude is None
+    if userLatitude is None or userLongitude is None:
+        userLatitude = request.json['userLat']
+        userLongitude = request.json['userLon']
+        preferredDistance = request.json['dist']
 
-	try:
-		assert (isThisAProductionRun == True)
-	except AssertionError:
-		userLatitude = request.json['userLat']
-		userLongitude = request.json['userLon']
-		preferredDistance = request.json['dist']
-	finally:
-		cursor = dbconnect.__change_data(query,preferredDistance)
-		return json.dumps(cursor)
+    query = "SELECT idroutes, route, ( 3959 * acos( cos( radians(" + str(userLatitude)
+    query += " ) ) * cos( radians( startPointLat ) ) * cos( radians( startPointLon ) - radians(" + str(userLongitude)
+    query += " ) ) + sin( radians(" + str(userLatitude) + ") ) * sin( radians( startPointLat ) ) ) ) "
+    query += "AS distance FROM routes HAVING distance < " + str(preferredDistance) + " ORDER BY distance LIMIT 0 , 20" 
+
+    try:
+        cursor = dbconnect.__change_data(query,preferredDistance)
+        return json.dumps(cursor)
+    except:
+        return codes.JSON_FAILURE
